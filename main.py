3f961229588221c386b85fa8d00b6b01aa24a71c
Andrey Okhotin <44405666+andrey-okhotin@users.noreply.github.com>
2024-08-24 15:50:10 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import argparse
+
+def main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-gpu', '--gpu', default='', type=str)
+    parser.add_argument('-port', '--port', default='8900', type=str)
+    args = parser.parse_args()
+    args.gpu = tuple(map(int, args.gpu.split('_')))
+    print(f"\n\n {args.gpu} \n\n", flush=True)
+    pass
+
+if __name__ == '__main__':    
+    main()
